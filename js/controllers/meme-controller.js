b61f70be4771eb508d958f1b42105406caab3846
orrans <orrans@gmail.com>
2025-11-12 13:45:32 +0200

    Resize canvas to picture size

--- js/controllers/meme-controller.js
+++ js/controllers/meme-controller.js
@@ -12,9 +12,13 @@ function onInit() {
 
 function renderMeme() {
     const meme = getMeme()
+    const elCanvasContainer = document.querySelector('.canvas-container')
     const elMemeImg = new Image()
     elMemeImg.src = meme.img.url
+    gElCanvas.width = elCanvasContainer.offsetWidth
+    gElCanvas.height = elCanvasContainer.offsetHeight
     elMemeImg.onload = function () {
+        resizeCanvasToImageSize(elMemeImg)
         gCtx.drawImage(elMemeImg, 0, 0, gElCanvas.width, gElCanvas.height)
         drawTopText(meme.lines[0].txt, meme.lines[0])
     }
@@ -33,4 +37,27 @@ function drawTopText(txt, line) {
 
     gCtx.fillText(txt, x, y)
     gCtx.strokeText(txt, x, y)
+}
+
+function resizeCanvasToImageSize(img) {
+    const maxWidth = 500
+    const maxHeight = 500
+
+    const imgRatio = img.naturalWidth / img.naturalHeight
+
+    let newWidth = img.naturalWidth
+    let newHeight = img.naturalHeight
+
+    if (newWidth > maxWidth) {
+        newWidth = maxWidth
+        newHeight = newWidth / imgRatio
+    }
+
+    if (newHeight > maxHeight) {
+        newHeight = maxHeight
+        newWidth = newHeight * imgRatio
+    }
+
+    gElCanvas.width = newWidth
+    gElCanvas.height = newHeight
 }
