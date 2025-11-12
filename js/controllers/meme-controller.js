e12d366d718e14d4fcc3f64be362d815c72728b4
Orran Bachar <orrans@users.noreply.github.com>
2025-11-12 13:22:24 +0200

    Add render meme option

--- js/controllers/meme-controller.js
+++ js/controllers/meme-controller.js
@@ -1,1 +1,36 @@
 'use strict'
+
+let gElCanvas
+let gCtx
+
+function onInit() {
+    gElCanvas = document.querySelector('.meme-canvas')
+    gCtx = gElCanvas.getContext('2d')
+    console.log(gCtx)
+    renderMeme(1) // test, need to delete after it
+}
+
+function renderMeme() {
+    const meme = getMeme()
+    const elMemeImg = new Image()
+    elMemeImg.src = meme.img.url
+    elMemeImg.onload = function () {
+        gCtx.drawImage(elMemeImg, 0, 0, gElCanvas.width, gElCanvas.height)
+        drawTopText(meme.lines[0].txt, meme.lines[0])
+    }
+}
+
+function drawTopText(txt, line) {
+    gCtx.lineWidth = 2
+    gCtx.strokeStyle = 'black'
+    gCtx.fillStyle = line.color
+    gCtx.font = `${line.size}px Impact`
+    gCtx.textAlign = 'center'
+    gCtx.textBaseline = 'top'
+
+    const x = gElCanvas.width / 2
+    const y = 10
+
+    gCtx.fillText(txt, x, y)
+    gCtx.strokeText(txt, x, y)
+}
--- js/services/meme-service.js
+++ js/services/meme-service.js
@@ -1,1 +1,31 @@
 'use strict'
+
+var gImgs = [{ id: 1, url: 'img/square-ratio/1.jpg', keywords: ['funny', 'cat'] }]
+
+var gMeme = {
+    selectedImgId: 1,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'I sometimes eat Falafel',
+            size: 20,
+            color: 'red',
+        },
+    ],
+}
+
+// demo search count for first load
+var gKeywordSearchCountMap = { funny: 12, cat: 16, baby: 2 }
+
+function getMeme(id) {
+    const meme = { ...gMeme }
+    meme.img = getImgById(meme.selectedImgId)
+    return meme
+}
+
+function getImgById(imgId) {
+    return gImgs.find((img) => img.id === imgId)
+}
+
+function setLineTxt() {}
+function setImg() {}
